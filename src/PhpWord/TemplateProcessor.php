08c1f94c425d4087fa9fc1ddcb8f824a994d453a
pringgojs <odyinggo@gmail.com>
2019-08-19 16:03:56 +0700

    fitur set value advanced

--- src/PhpWord/TemplateProcessor.php
+++ src/PhpWord/TemplateProcessor.php
@@ -1001,4 +1001,100 @@ class TemplateProcessor
 
         return $results;
     }
+
+    
+    /**
+     * Handling bug when setValue
+     * Usage. 
+     * $search_replace_array = array(
+     *       'ship'=> 'Verry', #inside a MS Word file ${kapal} will change to Verry
+     *       'date'=> '14 Juni 1995' #${date} will change to 14 Juni 1995
+     *  );
+     * $phpword->setValueAdvanced($search_replace_array);
+     */
+    public  function setValueAdvanced($search_replace)
+    {
+        foreach ($this->tempDocumentHeaders as $index => $headerXML) {
+            $this->tempDocumentHeaders[$index] = $this->setValueForPartAdvanced($this->tempDocumentHeaders[$index], $search_replace);
+        }
+
+        $this->tempDocumentMainPart = $this->setValueForPartAdvanced($this->tempDocumentMainPart, $search_replace);
+
+        foreach ($this->tempDocumentFooters as $index => $headerXML) {
+            $this->tempDocumentFooters[$index] = $this->setValueForPartAdvanced($this->tempDocumentFooters[$index], $search_replace);
+        }
+    }
+    
+    protected  function setValueForPartAdvanced($documentPartXML, $search_replace)
+    {
+        $pattern = '/<w:t>(.*?)<\/w:t>/';
+        $rplStringBeginOffcetsStack = array();
+        $rplStringEndOffcetsStack = array();
+        $rplCleanedStrings = array();
+        $stringsToClean = array();
+        preg_match_all($pattern, $documentPartXML, $words, PREG_OFFSET_CAPTURE);
+
+        $bux_founded = false;
+        $searching_started = false;
+        foreach($words[1] as $key_of_words => $word)
+        {
+            $exploded_chars = str_split($word[0]);
+            foreach($exploded_chars as $key_of_chars => $char)
+            {
+                if ($bux_founded)
+                {
+                    if ($searching_started)
+                    {
+                        if ($char == "}")
+                        {
+                            $bux_founded = false;
+                            $searching_started = false;
+                            array_push($rplStringEndOffcetsStack, ($word[1]+mb_strlen($word[0])+6));
+                        }
+                    }
+                    else
+                    {
+                        if ($char == "{")
+                        {
+                            $searching_started = true;
+                        }
+                        else
+                        {
+                            $bux_founded = false;
+                            array_pop($rplStringBeginOffcetsStack);
+                        }
+                    }
+                }
+                else
+                {
+                    if ($char == "$")
+                    {
+                        $bux_founded = true;
+                        array_push($rplStringBeginOffcetsStack, $word[1]-5);
+                    }
+                }
+            }
+        }
+        for($index=0; $index<count($rplStringEndOffcetsStack); $index++)
+        {
+            $string_to_clean = substr($documentPartXML, $rplStringBeginOffcetsStack[$index], ($rplStringEndOffcetsStack[$index]-$rplStringBeginOffcetsStack[$index]));
+            array_push($stringsToClean, $string_to_clean);
+            preg_match_all($pattern, $string_to_clean, $words_to_concat);
+            $cleaned_string = implode("", $words_to_concat[1]);
+            $cleaned_string = preg_replace('/[${}]+/', '', $cleaned_string);
+            array_push($rplCleanedStrings, $cleaned_string);
+        }
+        for ($index=0; $index<count($rplCleanedStrings); $index++)
+        {
+            foreach($search_replace as $key_search => $replace)
+            {
+                if ($rplCleanedStrings[$index] == $key_search)
+                {
+                    $documentPartXML = str_replace($stringsToClean[$index], "<w:t>".$replace."</w:t>", $documentPartXML);
+                    break;
+                }
+            }
+        }
+        return $documentPartXML;
+    }
 }
